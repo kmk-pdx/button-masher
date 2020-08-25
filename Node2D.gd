aa142079ffe62e7e7de524531740b0e37cc1a941
kmk-pdx <50334513+kmk-pdx@users.noreply.github.com>
2020-08-24 20:36:56 -0700

    Added sound

--- Node2D.gd
+++ Node2D.gd
@@ -2,20 +2,19 @@ extends Node2D
 
 onready var textbox = $CenterContainer/CatNameText
 onready var imagebox = $CenterContainer/Control/TextureRect
+onready var runningcat = $CenterContainer/Control/AnimatedSprite
+onready var buttonaudio = $AudioStreamPlayer
 var strings = ["momo", "mochi", "mori", "dorian", "moreau", "mika", "mort"]
 var images = ["res://momo.jpg", "res://icon.png", "res://mori.jpg", "res://icon.png", "res://moreau.jpg", "res://mika.jpg", "res://mort.jpg"]
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
 
 func _on_Button_pressed():
 	var choice = randi()%strings.size()
 	textbox.text = strings[choice]
 	imagebox.texture = load(images[choice])
+	buttonaudio.play()
+	if runningcat != null:
+		runningcat.queue_free()
+	
